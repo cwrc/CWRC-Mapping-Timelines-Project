ddde66b9dc50401538f37b6daf8ee4f1d3e7ba25
Robin Miller <robin@tenjin.ca>
2015-04-22 14:11:33 -0600

    Added spiderfy to the map pins

--- assets/javascripts/simile_override.js
+++ assets/javascripts/simile_override.js
@@ -215,6 +215,414 @@ Exhibit.TimelineView.prototype._select = function (selection) {
     }
 };
 
+// overridden to use spiderfy for stacks
+Exhibit.MapView.prototype._rePlotItems = function (unplottableItems) {
+    var self = this;
+    var collection = this._uiContext.getCollection();
+    var database = this._uiContext.getDatabase();
+    var settings = this._settings;
+    var accessors = this._accessors;
+    var currentSet = collection.getRestrictedItems();
+    var locationToData = {};
+    var hasColorKey = (accessors.getColorKey != null);
+    var hasSizeKey = (accessors.getSizeKey != null);
+    var hasIconKey = (accessors.getIconKey != null);
+    var hasIcon = (accessors.getIcon != null);
+    var hasPoints = (this._getLatlng != null);
+    var hasPolygons = (accessors.getPolygon != null);
+    var hasPolylines = (accessors.getPolyline != null);
+
+    var makeLatLng = settings.latlngOrder == "latlng" ? function (first, second) {
+        return new google.maps.LatLng(first, second);
+    } : function (first, second) {
+        return new google.maps.LatLng(second, first);
+    };
+
+    var colorCodingFlags = {mixed: false, missing: false, others: false, keys: new Exhibit.Set()};
+    var sizeCodingFlags = {mixed: false, missing: false, others: false, keys: new Exhibit.Set()};
+    var iconCodingFlags = {mixed: false, missing: false, others: false, keys: new Exhibit.Set()};
+    var bounds, maxAutoZoom = Infinity;
+
+    currentSet.visit(function (itemID) {
+        var latlngs = [];
+        var polygons = [];
+        var polylines = [];
+        if (hasPoints) {
+            self._getLatlng(itemID, database, function (v) {
+                if (v != null && "lat" in v && "lng" in v) {
+                    latlngs.push(v);
+                }
+            });
+        }
+        if (hasPolygons) {
+            accessors.getPolygon(itemID, database, function (v) {
+                if (v != null) {
+                    polygons.push(v);
+                }
+            });
+        }
+        if (hasPolylines) {
+            accessors.getPolyline(itemID, database, function (v) {
+                if (v != null) {
+                    polylines.push(v);
+                }
+            });
+        }
+        if (latlngs.length > 0 || polygons.length > 0 || polylines.length > 0) {
+            var color = self._settings.color;
+            var colorKeys = null;
+            if (hasColorKey) {
+                colorKeys = new Exhibit.Set();
+                accessors.getColorKey(itemID, database, function (v) {
+                    colorKeys.add(v);
+                });
+                color = self._colorCoder.translateSet(colorKeys, colorCodingFlags);
+            }
+            if (latlngs.length > 0) {
+                var sizeKeys = null;
+                if (hasSizeKey) {
+                    sizeKeys = new Exhibit.Set();
+                    accessors.getSizeKey(itemID, database, function (v) {
+                        sizeKeys.add(v);
+                    });
+                }
+                var iconKeys = null;
+                if (hasIconKey) {
+                    iconKeys = new Exhibit.Set();
+                    accessors.getIconKey(itemID, database, function (v) {
+                        iconKeys.add(v);
+                    });
+                }
+                for (var n = 0;
+                     n < latlngs.length;
+                     n++) {
+                    var latlng = latlngs[n];
+                    var latlngKey = latlng.lat + "," + latlng.lng;
+                    if (latlngKey in locationToData) {
+                        var locationData = locationToData[latlngKey];
+                        locationData.items.push(itemID);
+                        if (hasColorKey) {
+                            locationData.colorKeys.addSet(colorKeys);
+                        }
+                        if (hasSizeKey) {
+                            locationData.sizeKeys.addSet(sizeKeys);
+                        }
+                        if (hasIconKey) {
+                            locationData.iconKeys.addSet(iconKeys);
+                        }
+                    } else {
+                        var locationData = {latlng: latlng, items: [itemID]};
+                        if (hasColorKey) {
+                            locationData.colorKeys = colorKeys;
+                        }
+                        if (hasSizeKey) {
+                            locationData.sizeKeys = sizeKeys;
+                        }
+                        if (hasIconKey) {
+                            locationData.iconKeys = iconKeys;
+                        }
+                        locationToData[latlngKey] = locationData;
+                    }
+                }
+            }
+            for (var p = 0;
+                 p < polygons.length;
+                 p++) {
+                self._plotPolygon(itemID, polygons[p], color, makeLatLng);
+            }
+            for (var p = 0;
+                 p < polylines.length;
+                 p++) {
+                self._plotPolyline(itemID, polylines[p], color, makeLatLng);
+            }
+        } else {
+            unplottableItems.push(itemID);
+        }
+    });
+
+    var oms = new OverlappingMarkerSpiderfier(self._map, {keepSpiderfied: true});
+    var addMarkersAtLocation = function (locationData) {
+
+        for (var i = 0; i < locationData.items.length; i++) {
+            var item = locationData.items[i];
+
+            var itemCount = locationData.items.length;
+            if (!bounds) {
+                bounds = new google.maps.LatLngBounds();
+            }
+            var shape = self._settings.shape;
+            var color = self._settings.color;
+            if (hasColorKey) {
+                color = self._colorCoder.translateSet(locationData.colorKeys, colorCodingFlags);
+            }
+            var iconSize = self._settings.iconSize;
+            if (hasSizeKey) {
+                iconSize = self._sizeCoder.translateSet(locationData.sizeKeys, sizeCodingFlags);
+            }
+            var icon = null;
+            if (itemCount == 1) {
+                if (hasIcon) {
+                    accessors.getIcon(locationData.items[0], database, function (v) {
+                        icon = v;
+                    });
+                }
+            }
+            if (hasIconKey) {
+                icon = self._iconCoder.translateSet(locationData.iconKeys, iconCodingFlags);
+            }
+            var point = new google.maps.LatLng(locationData.latlng.lat, locationData.latlng.lng);
+            if (maxAutoZoom > locationData.latlng.maxAutoZoom) {
+                maxAutoZoom = locationData.latlng.maxAutoZoom;
+            }
+            bounds.extend(point);
+
+            var marker = Exhibit.MapView._makeMarker(point, shape, color, iconSize, icon, itemCount == 1 ? "" : itemCount.toString(), self._settings);
+            oms.addMarker(marker);
+            marker.cwrcData = [item];
+
+//          google.maps.event.addListener(marker, "click", function () {
+//              self._showInfoWindow(locationData.items, null, marker);
+//              if (self._selectListener != null) {
+//                  self._selectListener.fire({itemIDs: locationData.items});
+//              }
+//          });
+
+            marker.setMap(self._map);
+            self._overlays.push(marker);
+            for (var x = 0;
+                 x < locationData.items.length;
+                 x++) {
+                self._itemIDToMarker[locationData.items[x]] = marker;
+            }
+        }
+    };
+    try {
+        for (var latlngKey in locationToData) {
+            addMarkersAtLocation(locationToData[latlngKey]);
+        }
+
+        oms.addListener('click', function (marker, event) {
+            self._showInfoWindow(marker.cwrcData, null, marker);
+            if (self._selectListener != null) {
+                self._selectListener.fire({itemIDs: marker.cwrcData});
+            }
+        });
+    } catch (e) {
+        alert(e);
+    }
+    if (hasColorKey) {
+        var legendWidget = this._dom.legendWidget;
+        var colorCoder = this._colorCoder;
+        var keys = colorCodingFlags.keys.toArray().sort();
+        if (settings.colorLegendLabel !== null) {
+            legendWidget.addLegendLabel(settings.colorLegendLabel, "color");
+        }
+        if (colorCoder._gradientPoints != null) {
+            var legendGradientWidget = this._dom.legendGradientWidget;
+            legendGradientWidget.addGradient(this._colorCoder._gradientPoints);
+        } else {
+            for (var k = 0;
+                 k < keys.length;
+                 k++) {
+                var key = keys[k];
+                var color = colorCoder.translate(key);
+                legendWidget.addEntry(color, key);
+            }
+        }
+        if (colorCodingFlags.others) {
+            legendWidget.addEntry(colorCoder.getOthersColor(), colorCoder.getOthersLabel());
+        }
+        if (colorCodingFlags.mixed && legendWidget) {
+            legendWidget.addEntry(colorCoder.getMixedColor(), colorCoder.getMixedLabel());
+        }
+        if (colorCodingFlags.missing) {
+            legendWidget.addEntry(colorCoder.getMissingColor(), colorCoder.getMissingLabel());
+        }
+    }
+    if (hasSizeKey) {
+        var legendWidget = this._dom.legendWidget;
+        var sizeCoder = this._sizeCoder;
+        var keys = sizeCodingFlags.keys.toArray().sort();
+        if (settings.sizeLegendLabel !== null) {
+            legendWidget.addLegendLabel(settings.sizeLegendLabel, "size");
+        }
+        if (sizeCoder._gradientPoints != null) {
+            var points = sizeCoder._gradientPoints;
+            var space = (points[points.length - 1].value - points[0].value) / 5;
+            keys = [];
+            for (var i = 0;
+                 i < 6;
+                 i++) {
+                keys.push(Math.floor(points[0].value + space * i));
+            }
+            for (var k = 0;
+                 k < keys.length;
+                 k++) {
+                var key = keys[k];
+                var size = sizeCoder.translate(key);
+                legendWidget.addEntry(size, key, "size");
+            }
+        } else {
+            for (var k = 0;
+                 k < keys.length;
+                 k++) {
+                var key = keys[k];
+                var size = sizeCoder.translate(key);
+                legendWidget.addEntry(size, key, "size");
+            }
+            if (sizeCodingFlags.others) {
+                legendWidget.addEntry(sizeCoder.getOthersSize(), sizeCoder.getOthersLabel(), "size");
+            }
+            if (sizeCodingFlags.mixed) {
+                legendWidget.addEntry(sizeCoder.getMixedSize(), sizeCoder.getMixedLabel(), "size");
+            }
+            if (sizeCodingFlags.missing) {
+                legendWidget.addEntry(sizeCoder.getMissingSize(), sizeCoder.getMissingLabel(), "size");
+            }
+        }
+    }
+    if (hasIconKey) {
+        var legendWidget = this._dom.legendWidget;
+        var iconCoder = this._iconCoder;
+        var keys = iconCodingFlags.keys.toArray().sort();
+        if (settings.iconLegendLabel != null) {
+            legendWidget.addLegendLabel(settings.iconLegendLabel, "icon");
+        }
+        for (var k = 0;
+             k < keys.length;
+             k++) {
+            var key = keys[k];
+            var icon = iconCoder.translate(key);
+            legendWidget.addEntry(icon, key, "icon");
+        }
+        if (iconCodingFlags.others) {
+            legendWidget.addEntry(iconCoder.getOthersIcon(), iconCoder.getOthersLabel(), "icon");
+        }
+        if (iconCodingFlags.mixed) {
+            legendWidget.addEntry(iconCoder.getMixedIcon(), iconCoder.getMixedLabel(), "icon");
+        }
+        if (iconCodingFlags.missing) {
+            legendWidget.addEntry(iconCoder.getMissingIcon(), iconCoder.getMissingLabel(), "icon");
+        }
+    }
+    if (bounds && settings.autoposition && !this._shown) {
+        self._map.fitBounds(bounds);
+        if (self._map.getZoom > maxAutoZoom) {
+            self._map_setZoom(maxAutoZoom);
+        }
+    }
+    this._shown = true;
+};
+
+Exhibit.MapView.makeCanvasIcon = function (width, height, color, label, iconImg, iconSize, settings) {
+    var drawShadow = function (icon) {
+        var width = icon.width;
+        var height = icon.height;
+        var shadowWidth = width + height;
+        var canvas = document.createElement("canvas");
+        canvas.width = shadowWidth;
+        canvas.height = height;
+        var context = canvas.getContext("2d");
+        context.scale(1, 1 / 2);
+        context.translate(height / 2, height);
+        context.transform(1, 0, -1 / 2, 1, 0, 0);
+        context.fillRect(0, 0, width, height);
+        context.globalAlpha = settings.shapeAlpha;
+        context.globalCompositeOperation = "destination-in";
+        context.drawImage(icon, 0, 0);
+        return canvas;
+    };
+    var pin = settings.pin;
+    var pinWidth = settings.pinWidth;
+    var pinHeight = settings.pinHeight;
+    var lineWidth = 1;
+    var lineColor = settings.borderColor || "black";
+    var alpha = settings.shapeAlpha;
+    var bodyWidth = width - lineWidth;
+    var bodyHeight = height - lineWidth;
+    var markerHeight = height + (pin ? pinHeight : 0);
+    var radius;
+    var canvas = document.createElement("canvas");
+    canvas.width = width;
+    canvas.height = markerHeight;
+    var context = canvas.getContext("2d");
+    context.clearRect(0, 0, width, markerHeight);
+    context.beginPath();
+    if (settings && (settings.shape == "circle")) {
+        radius = bodyWidth / 2;
+        if (!pin) {
+            context.arc(width / 2, height / 2, radius, 0, 2 * Math.PI);
+        } else {
+            var meetAngle = Math.atan2(pinWidth / 2, bodyHeight / 2);
+            context.arc(width / 2, height / 2, radius, Math.PI / 2 + meetAngle, Math.PI / 2 - meetAngle);
+            context.lineTo(width / 2, height + pinHeight - lineWidth / 2);
+        }
+    } else {
+        radius = bodyWidth / 4;
+        var topY = leftX = lineWidth / 2;
+        var botY = height - lineWidth / 2;
+        var rightX = width - lineWidth / 2;
+        context.moveTo(rightX - radius, topY);
+        context.arcTo(rightX, topY, rightX, topY + radius, radius);
+        context.lineTo(rightX, botY - radius);
+        context.arcTo(rightX, botY, rightX - radius, botY, radius);
+        if (pin) {
+            context.lineTo(width / 2 + pinWidth / 2, botY);
+            context.lineTo(width / 2, height + pinHeight - lineWidth / 2);
+            context.lineTo(width / 2 - pinWidth / 2, botY);
+        }
+        context.lineTo(leftX + radius, botY);
+        context.arcTo(leftX, botY, leftX, botY - radius, radius);
+        context.lineTo(leftX, topY + radius);
+        context.arcTo(leftX, topY, leftX + radius, topY, radius);
+    }
+    context.closePath();
+    context.fillStyle = color;
+    context.globalAlpha = alpha;
+    context.fill();
+    if (iconImg) {
+        context.save();
+        context.clip();
+        context.globalAlpha = 1;
+        context.translate(width / 2 + settings.iconOffsetX, height / 2 + settings.iconOffsetY);
+        var scale;
+        var heightScale = 1 * height / iconImg.naturalHeight;
+        var widthScale = 1 * width / iconImg.naturalWidth;
+        switch (settings.iconFit) {
+            case"width":
+                scale = widthScale;
+                break;
+            case"height":
+                scale = heightScale;
+                break;
+            case"both":
+            case"larger":
+                scale = Math.min(heightScale, widthScale);
+                break;
+            case"smaller":
+                scale = Math.max(heightScale, widthScale);
+                break;
+        }
+        context.scale(scale, scale);
+        context.scale(settings.iconScale, settings.iconScale);
+        context.drawImage(iconImg, -iconImg.naturalWidth / 2, -iconImg.naturalHeight / 2);
+        context.restore();
+    }
+    context.strokeStyle = lineColor;
+    context.lineWidth = lineWidth;
+    context.stroke();
+    var shadow = drawShadow(canvas);
+    if (label) {
+        context.font = "8pt Arial";
+        context.textBaseline = "middle";
+        context.textAlign = "center";
+        context.globalAlpha = 1;
+        context.fillStyle = "black";
+        context.fillText(label, width / 2, height / 2, width / 1.4);
+    }
+    return{iconURL: canvas.toDataURL(), shadowURL: shadow.toDataURL()};
+};
+
 // TODO: Possible to override this one to make it do the custom centering that michael requested
 // Todo: we also should look into making a new HTML attribute for this.
 //Exhibit.TimelineView.prototype._reconstruct = function () {
