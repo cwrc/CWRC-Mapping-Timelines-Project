883ff954bc72937b0ac9d1e96256e5d87ccd8683
Robin Miller <robin@tenjin.ca>
2015-09-16 20:35:10 -0600

    Grid now sorts with multiple, user-defined properties

--- assets/javascripts/components/grid.js
+++ assets/javascripts/components/grid.js
@@ -1,5 +1,18 @@
 ko.components.register('grid', {
-    template: '<table>\
+    template: '<div>\
+                    Sort <span data-bind="visible: sortContexts().length > 0">by</span>\
+                    <!-- Building a "widget" for editing each sorting context-->\
+                    <div data-bind="foreach: sortContexts">\
+                        <div>\
+                            <select data-bind="options: $parent.allFields, optionsText: \'name\', optionsValue: \'name\', optionsCaption:\'Choose...\', value: $data.name"></select>\
+                            <a href="#" data-bind="click: function() { $data.reverse() }, text: $data.getFieldDirectionLabel()"></a>\
+                            <a href="#" data-bind="click: function() { $parent.removeSortBy($data) }">x</a>\
+                        </div>\
+                        , then\
+                    </div>\
+                    <a href="#" title="Add Sorting Rule" data-bind="click: addContext ">by...</a>\
+               </div>\
+               <table>\
                     <!-- explicit thead & tbody are important, otherwise the browser assumes incorrect things-->\
                     <thead>\
                         <tr>\
@@ -43,16 +56,108 @@ ko.components.register('grid', {
                </section>',
 
     /**
-     * A table represetnation of data.
+     * A table representation of data.
+     *
      * @param columns: Hash in the form of {ColumnLabel: 'fieldName', ColumnLabel2: 'fieldName2'} (Required)
      * @param nowrap: List of column names that should have line-wrap disabled.
      * @param pageSize: the number of items per page. Default: 10
+     * @param initialSortBy: an array of field names by which to sort, in order of priority. Use a suffix of -za to get reverse order. eg. ['startdate', 'longName-za']. Optional; if not given, then no sorting will be done.
      */
     viewModel: function (params) {
         var self = this;
 
-        self.items = CWRC.filteredData;
+        var SortContext = function (fieldString) {
+            var self = this;
+            var split = (fieldString || '').split('-');
+
+            self.name = ko.observable(split[0]);
+            self.direction = ko.observable((split[1] || 'az').toLowerCase());
+
+            self['getFieldDirectionLabel'] = function () {
+                return self.direction() == 'az' ? 'A->Z' : 'Z->A';
+            };
+
+            self['reverse'] = function () {
+                var index, reversedLabel, direction;
+
+                self.direction(self.direction() == 'az' ? 'za' : 'az');
+            };
+        };
+
+        // assumes all data objects have same format.
+        self.allFields = ko.pureComputed(function () {
+            return Object.keys(CWRC.rawData()[0]).map(function (fieldString) {
+                return new SortContext(fieldString);
+            });
+        });
+
+        self.addContext = function () {
+            self.sortContexts.push(new SortContext());
+        };
+
+        self['removeSortBy'] = function (fieldName) {
+            var index = self.sortContexts.indexOf(fieldName);
+            self.sortContexts.splice(index, 1);
+        };
+
         self.columns = params['columns'];
+        self.sortContexts = ko.observableArray(params['initialSortBy'].map(function (fieldString) {
+            return new SortContext(fieldString);
+        }));
+
+        self.items = ko.pureComputed(function () {
+            var unsorted, direction, nameAndDirection, result, sortContexts, fieldName, sortFunction, collator, sortContext;
+
+            unsorted = Array.prototype.slice.call(CWRC.filteredData());
+            sortContexts = self.sortContexts();
+            result = 0;
+
+            try {
+                collator = new Intl.Collator('en', {
+                    sensitivity: 'base',
+                    ignorePunctuation: true
+                });
+            } catch (e) {
+                collator = null
+            }
+
+            sortFunction = function (a, b) {
+                // Find the first sort criteria that gives a non-same result,
+                // (That's why we need "break" here, and cannot use Array's forEach() )
+                for (var i = 0; i < sortContexts.length; i++) {
+                    sortContext = sortContexts[i];
+
+                    fieldName = sortContext.name();
+
+                    if (!fieldName)
+                        break;
+
+                    // collator doesn't exist in all platforms. Fall back to (much) slower localeCompare
+                    if (collator) {
+                        result = collator.compare(a[fieldName].trim(), b[fieldName].trim());
+                    } else {
+                        result = a[fieldName].trim().localeCompare(b[fieldName].trim(), 'en', {
+                            sensitivity: 'base',
+                            ignorePunctuation: true
+                        });
+                    }
+
+                    if (sortContext.direction() == 'za')
+                        result *= -1;
+
+                    if (result != 0)
+                        break;
+                }
+
+                return result;
+            };
+
+            if (sortContexts.length > 0) {
+                return unsorted.sort(sortFunction);
+            } else {
+                return unsorted;
+            }
+        });
 
         self.currentPageIndex = ko.observable(1);
         self.pageSize = params.pageSize || 20;
@@ -63,12 +168,12 @@ ko.components.register('grid', {
             return self.items().slice(startIndex, startIndex + self.pageSize);
         });
 
-        self.maxPageIndex = ko.computed(function () {
+        self.maxPageIndex = ko.pureComputed(function () {
             return Math.ceil(self.items().length / self.pageSize);
         });
 
         self.pageNeighbourhoodDistance = 1;
-        self.pageNeighbourhood = ko.computed(function () {
+        self.pageNeighbourhood = ko.pureComputed(function () {
             var low = Math.max(1, self.currentPageIndex() - self.pageNeighbourhoodDistance);
             var high = Math.min(self.maxPageIndex(), self.currentPageIndex() + self.pageNeighbourhoodDistance);
 
--- assets/javascripts/components/switcher.js
+++ assets/javascripts/components/switcher.js
@@ -3,11 +3,11 @@ ko.components.register('switcher', {
                     <a href="#" data-bind="click: function() { setView(\'map_view\') }, attr:{selected: isView(\'map_view\')}">\
                         Map View\
                     </a>\
-                    •\
+                    <!--\
                     <a href="#" data-bind="click: function() { setView(\'list_view\') }, attr:{selected: isView(\'list_view\')}">\
                         List View\
                     </a>\
-                    •\
+                    -->\
                     <a href="#" data-bind="click: function() { setView(\'table_view\') }, attr:{selected: isView(\'table_view\')}">\
                         Grid View\
                     </a>\
@@ -22,11 +22,13 @@ ko.components.register('switcher', {
                                     markerHeight: 18">\
                     </map>\
                </section>\
+               <!--\
                <section id="list_view" data-bind="visible: isView(\'list_view\')">\
                     <list></list>\
                </section>\
+               -->\
                <section id="table_view" data-bind="visible: isView(\'table_view\')">\
-                    <grid params="columns:{Title: \'longLabel\', Collection: \'group\', Location: \'location\', Start: \'startDate\', End: \'endDate\'}"></grid>\
+                    <grid params="columns: {Title: \'longLabel\', Collection: \'group\', Location: \'location\', Start: \'startDate\', End: \'endDate\'}, initialSortBy: [\'group\', \'longLabel-za\']"></grid>\
                </section>',
 
     viewModel: function () {
