5943c3b062a3884c15c3f41c54405257a383f7da
Robin Miller <robin@tenjin.ca>
2015-10-01 13:29:38 -0600

    Generalized tab switching pane

---
+++ assets/javascripts/components/tab_pane.js
@@ -0,0 +1,37 @@
+ko.components.register('tab_pane', {
+    template: {element: 'tab-pane-template'},
+    viewModel: {
+        // See http://knockoutjs.com/documentation/component-custom-elements.html#passing-markup-into-components
+        // for why this is a little different from most components
+        createViewModel: function (params, componentInfo) {
+            var SwitcherModel = function (params, componentInfo) {
+                var self = this;
+
+                self.views = componentInfo;
+
+                // switcher state
+                self.currentView = ko.observable(0);
+
+                self['setView'] = function (name) {
+                    self.currentView(name);
+                };
+
+                self['isView'] = function (name) {
+                    return self.currentView() == name;
+                };
+
+                self['tabName'] = function (node, index) {
+                    return node.getAttribute('data-tab-label') || 'Tab ' + (index + 1)
+                }
+            };
+
+            var switchableNodes;
+
+            switchableNodes = componentInfo.templateNodes.filter(function (node) {
+                return node.nodeType == Node.ELEMENT_NODE;
+            });
+
+            return new SwitcherModel(params, switchableNodes)
+        }
+    }
+});
