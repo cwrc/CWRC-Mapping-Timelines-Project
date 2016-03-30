ba70534add091430511fb5077669e521133c2547
Robin Miller <robin@tenjin.ca>
2016-03-29 18:26:16 -0600

    Export to JSON added.

--- assets/javascripts/components/filters/filter_save_results.js
+++ assets/javascripts/components/filters/filter_save_results.js
@@ -7,8 +7,28 @@ ko.components.register('filter_save_results', {
         var self = this;
 
         self['save'] = function () {
-            alert('save')
+            var data;
+
+            data = JSON.stringify(CWRC.filteredData());
+
+            self.triggerDownload('results.json', 'application/json', data);
+            //self.triggerDownload('text/csv', data);
+            //self.triggerDownload('application/json', data); // json-LD
+            //self.triggerDownload('application/xml', data); //
         };
+
+        self['triggerDownload'] = function (name, mime, data) {
+            /**
+             * I'm not a big fan of library bloat, especially those that add global garbage like this,
+             * but at the time of writing, the support for Blobs and the FileSystem API is too spotty.
+             * FileSaver.js seems to be the only independent solution with roughly broad support.
+             *
+             * Data URIs don't seem to always work for large datasets, otherwise that would be preferred.
+             *
+             * - Robin Miller
+             */
+            saveTextAs(data, name);
+        }
     }
 })
 ;
