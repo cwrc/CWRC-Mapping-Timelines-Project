f817e9581134093f9d3b75ea0af99b8f66ec396d
Robin Miller <robin@tenjin.ca>
2016-03-30 13:38:10 -0600

    Added CSV support to export

--- assets/javascripts/components/filters/filter_save_results.js
+++ assets/javascripts/components/filters/filter_save_results.js
@@ -1,20 +1,90 @@
 ko.components.register('filter_save_results', {
-    template: '<input type="button" data-bind="value: \'Save Results...\', click: function(){ save() }"/>',
+    template: ' <input type="button" data-bind="value: \'Save Results...\', click: toggleDialog"/>\
+                <div class="save-dialog" data-bind="visible: dialogVisible, click: toggleDialog">\
+                    <div data-bind="click: function(){}, clickBubble: false">\
+                        <header>Download As</header>\
+                        <input type="button" data-bind="value: \'JSON\', click: function(){ saveJSON() }"/>\
+                        <input type="button" data-bind="value: \'CSV\', click: function(){ saveCSV() }"/>\
+                        <hr>\
+                        <input type="button" data-bind="value: \'Cancel\', click: toggleDialog"/>\
+                    </div>\
+                </div>',
 
     /**
      */
     viewModel: function () {
         var self = this;
 
-        self['save'] = function () {
-            var data;
+        self.dialogVisible = ko.observable(false);
 
-            data = JSON.stringify(CWRC.filteredData());
+        self['toggleDialog'] = function () {
+            self.dialogVisible(!self.dialogVisible());
+        };
 
-            self.triggerDownload('results.json', 'application/json', data);
-            //self.triggerDownload('text/csv', data);
-            //self.triggerDownload('application/json', data); // json-LD
-            //self.triggerDownload('application/xml', data); //
+        self['saveJSON'] = function () {
+            self.triggerDownload('results.json', 'application/json', JSON.stringify(CWRC.filteredData()));
+        };
+
+        self['saveCSV'] = function () {
+            var headers, lines, line, csv, fileContent;
+
+            headers = [];
+
+            csv = {
+                separatorOptions: [',', ';', '\t', '|'],
+                separatorId: 0,
+                separator: function () {
+                    return this.separatorOptions[this.separatorId];
+                },
+                next: function () {
+                    this.separatorId++;
+                },
+                separatorError: function () {
+                    return this.separatorId >= this.separatorOptions.length
+                }
+            };
+
+            // Need to collect all possible headers separately so that we capture all entries in correct order
+            CWRC.filteredData().forEach(function (item) {
+                Object.keys(item).forEach(function (key) {
+                    if (headers.indexOf(key) < 0)
+                        headers.push(key)
+                });
+            });
+
+            lines = [headers];
+
+            CWRC.filteredData().forEach(function (item) {
+                lines.push(headers.map(function (key) {
+                    if (item[key] && item[key].indexOf(csv.separator()) >= 0)
+                        csv.next();
+
+                    return item[key] || '';
+                }));
+            });
+
+            fileContent = lines.reduce(function (lines, line) {
+                lines.push(line.join(csv.separator()));
+                return lines;
+            }, []).join('\n');
+
+            if (csv.separatorError()) {
+                alert('Cannot save as CSV. All possible separators ("' + csv.separatorOptions.join('" "') + '") are contained in the data.')
+            } else {
+                self.triggerDownload('results.csv', 'text/csv', fileContent);
+            }
+        };
+
+        self['saveJSONLD'] = function () {
+            alert('make JSONLD by using magic')
+
+            //self.triggerDownload('results.jsonld', 'application/json', data);
+        };
+
+        self['saveXML'] = function () {
+            alert('make XML data by iterating over the objects, then their keys and inventing tags as needed')
+
+            //self.triggerDownload('results.xml', 'application/xml', data); //
         };
 
         self['triggerDownload'] = function (name, mime, data) {
@@ -28,7 +98,8 @@ ko.components.register('filter_save_results', {
              * - Robin Miller
              */
             saveTextAs(data, name);
+
+            self.dialogVisible(false);
         }
     }
-})
-;
+});
