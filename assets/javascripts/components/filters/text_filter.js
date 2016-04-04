5d1b4030d39af10672ee13b9a63d829480e41e6d
Robin Miller <robin@tenjin.ca>
2016-04-04 17:43:48 -0600

    Extracted text filter history support into KO extender

--- assets/javascripts/components/filters/text_filter.js
+++ assets/javascripts/components/filters/text_filter.js
@@ -1,3 +1,61 @@
+/**
+ * Knockout extender to enhance the behaviour of an observable.
+ *
+ * @param target the Knockout observable to be extended
+ * @param params Includes:
+ *                  - label: the name of the state
+ *                  - querySymbol: the variable name for the query string value
+ * @returns {*} extended target observable
+ */
+ko.extenders.history = function (target, params) {
+    target.__updatingFromHistory__ = false;
+
+    History.Adapter.bind(window, 'statechange', function () {
+        var data;
+
+        data = History.getState().data[params.querySymbol];
+
+        //console.log('READ ' + params.label + ':')
+        //console.log(state.data);
+        //console.log('')
+
+        if (target != data) {
+            target.__updatingFromHistory__ = true;
+            target(data || '');
+            target.__updatingFromHistory__ = false;
+        }
+    });
+
+    target.subscribe(function (newVal) {
+        var stateData, stateLabel, stateUri;
+
+        if (target.__updatingFromHistory__)
+            return;
+
+        // TODO: replace the value in the query string & data packet, or remove it entirely if it is empty.
+        if (newVal.length > 0) {
+            stateLabel = params.label + ' "' + newVal + '" - ';
+            stateUri = '?' + params.querySymbol + '=' + newVal;
+        } else {
+            stateLabel = '';
+            stateUri = '?';
+        }
+
+        stateData = {};
+        if (newVal)
+            stateData[params.querySymbol] = newVal;
+
+        //console.log('SAVE ' + params.label + ' Filter:')
+        //console.log(stateData)
+        //console.log('')
+
+        History.pushState(stateData, stateLabel + 'PlotIt', stateUri)
+    });
+
+    return target;
+};
+
+
 ko.components.register('text_filter', {
     template: '<header>\
                     <span data-bind="text: label"></span>\
@@ -18,55 +76,12 @@ ko.components.register('text_filter', {
         // Using timeouts to throttle the filtering, otherwise it becomes sluggish
         self.filterText = ko.observable(CWRC.Network.getParam('s') || '').extend({
             method: 'notifyWhenChangesStop',
-            rateLimit: 300
-        });
-
-        self.updating = false;
-        History.Adapter.bind(window, 'statechange', function () {
-            var state = History.getState();
-            var data = state.data['s'];
-
-            //if (data == undefined)
-            //    return;
-
-            //console.log('READ ' + self.label + ':')
-            //console.log(state.data);
-            //console.log('')
-
-            if (self.filterText() != data) {
-                self.updating = true;
-                self.filterText(data || '');
-                self.updating = false;
+            rateLimit: 300,
+            history: {
+                label: self.label,
+                querySymbol: 's'
             }
         });
-
-        self.filterText.subscribe(function (newVal) {
-            var stateData, stateLabel, stateUri, fieldName;
-
-            fieldName = 's';
-
-            if (self.updating)
-                return;
-
-            if (newVal.length > 0) {
-                stateLabel = self.label + ' "' + newVal + '" - ';
-                stateUri = '?' + fieldName + '=' + newVal;
-            } else {
-                stateLabel = '';
-                stateUri = '?';
-            }
-
-            stateData = {};
-            if (newVal)
-                stateData[fieldName] = newVal;
-
-            //console.log('SAVE ' + self.label + ' Filter:')
-            //console.log(stateData)
-            //console.log('')
-
-            History.pushState(stateData, stateLabel + 'PlotIt', stateUri)
-        });
-
 
         self['reset'] = function () {
             self.filterText('');
