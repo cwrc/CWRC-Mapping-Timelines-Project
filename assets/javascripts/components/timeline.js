8ae8bea35f45341b13c2f15116e07c47cd8765b2
Robin Miller <robin@tenjin.ca>
2016-04-28 14:35:20 -0600

    Added loading status to long row calc

--- assets/javascripts/components/timeline.js
+++ assets/javascripts/components/timeline.js
@@ -309,9 +309,11 @@ CWRC.Timeline.DEFAULT_SCALE_STEP = 1.25;
 
         // TODO: this is a hack to force the correct order. It non-deterministically isn't updaitng rows on load all the time
         window.setTimeout(function () {
+            CWRC.isLoading(true);
             self.tokens().forEach(function (token) {
                 token.updateRow();
             });
+            CWRC.isLoading(false);
         }, 100)
 
     };
