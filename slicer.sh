a46b884214494288390999c9cb655393c22325d1
Robin Miller <robin@tenjin.ca>
2016-07-06 16:05:18 -0600

    Slicer now supports taking arguments to slice an arbitary image

--- slicer.sh
+++ slicer.sh
@@ -1,24 +1,43 @@
 #!/usr/bin/env bash
 
+# USAGE
+#
+# slicer.sh <input file> <north bounding> <east bounding> <south bounding> <west bounding>
+#
+# Boundings are latitudes (north & south) or longitudes (east & west) that define the area
+# over which the map should display
+#
 # Dependencies:
 #
 # **imagemagick**
-# **gdal**
+#    sudo apt-get install imagemagick
 #
-# sudo add-apt-repository ppa:ubuntugis/ppa && sudo apt-get update
-# sudo apt-get install gdal-bin
-# sudo apt-get install python-gdal
+# You may need to first add the main repository:
+#    sudo add-apt-repository main
+#
+# **gdal**
+#    sudo add-apt-repository ppa:ubuntugis/ppa && sudo apt-get update
+#    sudo apt-get install gdal-bin
+#    sudo apt-get install python-gdal
 #
 #
 # You may want to read http://webdesign.tutsplus.com/tutorials/the-google-maps-api-and-custom-overlays--webdesign-13868
 # to get an idea of what's going on.
 
-input_file=assets/images/maps/BNA_1854.png
+input_file=$1
+
+if [ "$#" -ne 5 ]
+then
+  echo -e "Not enough arguments provided.\n"
+  echo -e "Usage: slicer.sh <input file> <north bounding> <east bounding> <south bounding> <west bounding>\n"
+  echo "Boundings are latitudes (N,S) or longitudes (E,W)"
+  exit 1
+fi
 
 image_name=$(basename $input_file | cut -f 1 -d '.')
 output_dir="assets/images/maps/tiles/$image_name"
 
-echo "Outputting to $output_dir"
+echo -e "Outputting to $output_dir\n"
 
 image_width=$(identify -format "%w" $input_file)
 image_height=$(identify -format "%h" $input_file)
@@ -30,10 +49,16 @@ wgs_coord_file="$tmp_dir/wgs84.vrt"
 google_maps_coord_file="$tmp_dir/google_coord.vrt"
 
 # NS are Latitudes, EW are longitudes
-bound_n=81.69
-bound_e=-17.58
-bound_s=27.87
-bound_w=-181.56
+bound_n=$2
+bound_e=$3
+bound_s=$4
+bound_w=$5
+
+echo "North: $bound_n degrees North"
+echo "East: $bound_e degrees East"
+echo "South: $bound_s degress North"
+echo -e "West: $bound_w degrees East\n"
+
 
 # inject Ground Control Points (GCPs) and export as a new file that contains that information.
 #
@@ -53,7 +78,8 @@ gdalwarp -of VRT -t_srs EPSG:3857 $wgs_coord_file $google_maps_coord_file
 #bound_w=-910307.6791052371
 #gdal_translate -of VRT -a_srs EPSG:3857 -gcp 0 0 $bound_w $bound_n -gcp $image_width 0 $bound_e $bound_n -gcp $image_width $image_height $bound_e bounds_s $input_file $google_maps_coord_file
 
-# the -z arg is for the zoom range to support in Google maps. Min possible zoom is 0, max is 21
-gdal2tiles.py -p mercator -z 0-4 $google_maps_coord_file $output_dir
+# --zoom means the zoom range to support in Google maps; min possible zoom is 0, max is 21
+# --resume is there to avoid clobbering existing data. If the existing data is desired, delete it manually.
+gdal2tiles.py --resume --profile mercator --zoom 0-2 $google_maps_coord_file $output_dir
 
 rm -rf $tmp_dir
